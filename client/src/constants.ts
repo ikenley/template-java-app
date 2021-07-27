122f8d917eaac7c76057d4f56608a99683ad635e
ikenley <ikenley6@gmail.com>
2021-07-26 20:33:36 -0400

    GOOGLE_LOGIN_CLIENT_ID

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,6 +1,8 @@
-export const { GOOGLE_LOGIN_CLIENT_ID } = process.env;
+const { GOOGLE_LOGIN_CLIENT_ID } = process.env;
 
 export const todo = "todo";
 
 const d = new Date();
 export const Today = new Date(d.getFullYear(), d.getMonth(), d.getDate());
+
+export const GoogleLoginClientId = GOOGLE_LOGIN_CLIENT_ID || "";
