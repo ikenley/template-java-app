bf384e98d17a4b609aa0e4baf378727bdfbd4256
ikenley <ikenley6@gmail.com>
2021-07-30 10:36:16 -0400

    jwt parsing

--- api/src/main/java/com/ikenley/templatejavaapp/auth/AuthorizationController.java
+++ api/src/main/java/com/ikenley/templatejavaapp/auth/AuthorizationController.java
@@ -2,17 +2,28 @@ package com.ikenley.templatejavaapp.auth;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 @RestController
 public class AuthorizationController {
+    private static final Logger logger = LoggerFactory.getLogger(AuthorizationController.class);
 
     @Value("${test.property}")
     private String testValue;
 
     // Simple check of whether user is authenticated
     @GetMapping("/authorization")
-    public String status() {
+    public String status(@AuthenticationPrincipal Jwt jwt) {
+        // String holderName = jwt.getClaimAsString("name");
+        // String email = jwt.getClaimAsString("email");
+        String userId = jwt.getSubject();
+
+        logger.info("/authorization jwt {} userId {}", jwt, userId);
+
         return "valid";
     }
 
--- api/src/main/java/com/ikenley/templatejavaapp/configuration/SecurityConfig.java
+++ api/src/main/java/com/ikenley/templatejavaapp/configuration/SecurityConfig.java
@@ -1,18 +1,12 @@
 package com.ikenley.templatejavaapp.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 // @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    // protected void configure(final HttpSecurity http) throws Exception {
-    // http.antMatcher("/**").authorizeRequests().antMatchers("/status").permitAll().anyRequest().authenticated())
-    // .oauth2ResourceServer(oauth2 -> oauth2.jwt();
-    // ;
-    // }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(authz -> authz.antMatchers("/status").permitAll().anyRequest().authenticated())
